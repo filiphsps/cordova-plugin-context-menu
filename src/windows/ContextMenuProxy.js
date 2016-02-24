792a8a8a496a6b4e5d988f8b1ac9e6330828d3c6
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-02-24 15:39:18 +0100

    Add minor improvement.

--- src/windows/ContextMenuProxy.js
+++ src/windows/ContextMenuProxy.js
@@ -1,18 +1,8 @@
-function scalePos(X, Y, W, H) {
-    var zoom = document.documentElement.msContentZoomFactor;
-    return {
-        x: (X + document.documentElement.scrollLeft - window.pageXOffset) * zoom,
-        y: (Y + document.documentElement.scrollTop - window.pageYOffset) * zoom,
-        width: W * zoom,
-        height: H * zoom
-    };
-}
-
 cordova.commandProxy.add("ContextMenu", {
     open: function (successCallback, errorCallback, para) {
         var title = para.title;
         var items = para.items;
-        var pos = scalePos(para.x, para.y, para.width, para.height);
+        var pos = {x: para.x, y: para.y};
         
         var contextMenu = new Windows.UI.Popups.PopupMenu();
         for(var n = 0; items.length > n; n++) {
@@ -21,12 +11,12 @@ cordova.commandProxy.add("ContextMenu", {
             contextMenu.commands.append(new Windows.UI.Popups.UICommand(item.title, null, n+1));
         }
         
-        menu.showForSelectionAsync(pos.done(function (invokedCommand) {
+        contextMenu.showAsync(pos).done(function (invokedCommand) {
             if (invokedCommand !== null) {
                 successCallback(items[invokedCommand.id-1].id);
             } else {
                 errorCallback('dismissed');
             }
-        }));
+        });
     }
 });
